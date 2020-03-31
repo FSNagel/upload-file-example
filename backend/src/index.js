507011430e16c8be5092dc1854bac3f90912fdee
Felipe Nagel <felipesnagel@gmail.com>
2020-03-31 14:02:28 -0300

    backend

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const routes = require('./routes');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+/**
+ * Database setup
+ *  */
+
+mongoose.connect(
+    process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(
+    '/files',
+    express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+)
+
+app.use(routes);
+
+
+app.listen(3000);
